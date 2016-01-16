1f5f61dc65d20db265cf8c6ca02215111b9e55c1
Wei Tang <hi@beyond.codes>
2016-01-16 17:40:26 +0800

    update panic function

--- src/lib.rs
+++ src/lib.rs
@@ -22,4 +22,9 @@ pub extern fn rust_main() {
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() { }
-#[lang = "panic_fmt"] extern fn panic_fmt() -> ! {loop { }}
+#[lang = "panic_fmt"]
+extern fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
+    println!("\n\nPANIC in {} at line {}:", file, line);
+    println!("    {}", fmt);
+    loop{}
+}

dd251ab254238ed0dd707b256100d35b8d234c25
Wei Tang <hi@beyond.codes>
2016-02-03 11:48:48 +0800

    Delete unused pages

--- src/cap/page_table.rs
+++
@@ -1,7 +0,0 @@
-// You can only access active page table using the recursive trick. Other page
-// tables need to be temporarily mapped in order to be accessible.
-
-pub struct PageTableCapability {
-    start: PhysicalAddress,
-    physical_start: PhysicalAddress,
-}

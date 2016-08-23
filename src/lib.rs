c5edccf7b319a463e4f9336f383a79b242a02a42
Wei Tang <hi@that.world>
2016-08-23 17:21:19 +0200

    Print hello world again

--- src/lib.rs
+++ src/lib.rs
@@ -22,5 +22,18 @@ mod logging;
 pub fn kmain()
 {
 	log!("Hello world! 1={}", 1);
+    
+    let hello = b"Hello World!";
+    let color_byte = 0x1f; // white foreground, blue background
+
+    let mut hello_colored = [color_byte; 24];
+    for (i, char_byte) in hello.into_iter().enumerate() {
+        hello_colored[i*2] = *char_byte;
+    }
+
+    // write `Hello World!` to the center of the VGA text buffer
+    let buffer_ptr = (0xFFFFFFFF80000000 + 0xb8000 as u64 + 1988) as *mut _;
+    unsafe { *buffer_ptr = hello_colored };
+    
 	loop {}
 }

e9f5fa816fad33c3431a6ae0d2f7f55a5f89aa51
Wei Tang <hi@that.world>
2016-08-25 03:00:22 +0200

    Fetch necessary information to allow creating GP

--- src/arch/x86_64/mod.rs
+++ src/arch/x86_64/mod.rs
@@ -21,4 +21,27 @@ pub mod debug;
 extern {
     pub static multiboot_sig: u32;
     pub static multiboot_ptr: u64;
+
+    static init_pml4: u64;
+    static init_stack_base: u64;
+    static init_stack: u64;
+    static kernel_end: u64;
+}
+
+use common::{PAddr, KERNEL_BASE};
+
+pub fn kernel_end_address() -> PAddr {
+    ((&kernel_end as *const _) as PAddr) - KERNEL_BASE
+}
+
+pub fn kernel_stack_address() -> PAddr {
+    ((&init_stack as *const _) as PAddr) - KERNEL_BASE
+}
+
+pub fn kernel_stack_init_base_address() -> PAddr {
+    ((&init_stack_base as *const _) as PAddr) - KERNEL_BASE
+}
+
+pub fn kernel_stack_max_base_address() -> PAddr {
+    ((&init_pml4 as *const _) as PAddr) - KERNEL_BASE
 }
--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,11 @@ pub fn kmain()
     
     log!("multiboot_sig: 0x{:x}", multiboot_sig);
     log!("multiboot_ptr: 0x{:x}", multiboot_ptr);
+
+    log!("kernel_stack: 0x{:x}", arch::kernel_stack_address());
+    log!("kernel_stack_init_base: 0x{:x}", arch::kernel_stack_init_base_address());
+    log!("kernel_stack_max_base: 0x{:x}", arch::kernel_stack_max_base_address());
+    log!("kernel_end: 0x{:x}", arch::kernel_end_address());
     
     let bootinfo = unsafe {
         multiboot::Multiboot::new(multiboot_ptr as u64, |addr, size| {

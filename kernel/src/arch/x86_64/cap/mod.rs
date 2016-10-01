dc36aed659e215ae461a4520ae48cd3b76c6e3d5
Wei Tang <hi@that.world>
2016-10-01 18:33:05 +0800

    add ThreadRuntime

--- kernel/src/arch/x86_64/cap/mod.rs
+++ kernel/src/arch/x86_64/cap/mod.rs
@@ -1,6 +1,8 @@
 mod paging;
+mod thread;
 
 pub use self::paging::{PageHalf, PML4Half, PDPTHalf, PDHalf, PTHalf};
+pub use self::thread::{ThreadRuntime};
 
 #[derive(Debug)]
 pub enum ArchSpecificCapability {

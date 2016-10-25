8bb028e85c0795370a25d74b4d0662d910b482c2
Wei Tang <hi@that.world>
2016-10-26 01:28:12 +0800

    IndexedShared Read/Write Guard

--- kernel/src/util/guard.rs
+++ kernel/src/util/guard.rs
@@ -1,5 +1,5 @@
 use super::{MemoryObject};
-use core::ops::{Deref, DerefMut};
+use core::ops::{Deref, DerefMut, Index, IndexMut};
 
 pub use spin::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
@@ -21,6 +21,16 @@ pub struct SharedReadGuard<'a, T: 'a> {
 pub struct SharedWriteGuard<'a, T: 'a> {
     lock: RwLockWriteGuard<'a, T>,
     object: MemoryObject<RwLock<T>>
+}
+
+pub struct IndexedSharedReadGuard<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T>> {
+    guard: SharedReadGuard<'a, U>,
+    index: I,
+}
+
+pub struct IndexedSharedWriteGuard<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T> + IndexMut<I>> {
+    guard: SharedWriteGuard<'a, U>,
+    index: I,
 }
 
 // Implementation for UniqueReadGuard
@@ -118,5 +128,53 @@ impl<'a, T: 'a> Deref for SharedWriteGuard<'a, T> {
 impl<'a, T: 'a> DerefMut for SharedWriteGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut T {
         self.lock.deref_mut()
+    }
+}
+
+// Implementation for IndexedSharedReadGuard
+
+impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T>> IndexedSharedReadGuard<'a, T, I, U> {
+    pub unsafe fn new(guard: SharedReadGuard<'a, U>, index: I) -> Self {
+        IndexedSharedReadGuard {
+            guard: guard,
+            index: index,
+        }
+    }
+}
+
+unsafe impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T>> Send for IndexedSharedReadGuard<'a, T, I, U> { }
+unsafe impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T>> Sync for IndexedSharedReadGuard<'a, T, I, U> { }
+
+impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T>> Deref for IndexedSharedReadGuard<'a, T, I, U> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.guard[self.index]
+    }
+}
+
+// Implementation for IndexedSharedWriteGuard
+
+impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T> + IndexMut<I>> IndexedSharedWriteGuard<'a, T, I, U> {
+    pub unsafe fn new(guard: SharedWriteGuard<'a, U>, index: I) -> Self {
+        IndexedSharedWriteGuard {
+            guard: guard,
+            index: index,
+        }
+    }
+}
+
+unsafe impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T> + IndexMut<I>> Send for IndexedSharedWriteGuard<'a, T, I, U> { }
+unsafe impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T> + IndexMut<I>> Sync for IndexedSharedWriteGuard<'a, T, I, U> { }
+
+impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T> + IndexMut<I>> Deref for IndexedSharedWriteGuard<'a, T, I, U> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.guard[self.index]
+    }
+}
+
+impl<'a, T: 'a, I: Copy, U: 'a + Index<I, Output=T> + IndexMut<I>> DerefMut for IndexedSharedWriteGuard<'a, T, I, U> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.guard[self.index]
     }
 }

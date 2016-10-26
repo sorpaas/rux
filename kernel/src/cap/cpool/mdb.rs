3b5dd34b2071c9538763c8552cc6cab24bdec083
Wei Tang <hi@that.world>
2016-10-26 18:37:47 +0800

    WIP: Finish MDB Iterators

--- kernel/src/cap/cpool/mdb.rs
+++ kernel/src/cap/cpool/mdb.rs
@@ -1,5 +1,5 @@
 use cap::{CPoolHalf, Cap, CPool, CapWriteObject, CapReadObject};
-use util::{SharedReadGuard, SharedWriteGuard, RefGuard, RefMutGuard, IndexedSharedReadGuard, IndexedSharedWriteGuard, Streamer};
+use util::{SharedReadGuard, SharedWriteGuard, RefGuard, RefMutGuard, IndexedSharedReadGuard, IndexedSharedWriteGuard};
 use core::ops::{Deref, DerefMut};
 use core::marker::{PhantomData};
 
@@ -58,6 +58,22 @@ pub struct MDB<'a> {
     holding_parent: Option<&'a mut MDB<'a>>
 }
 
+fn match_mdbs_mut<'a, 'b>(cap: &'b mut Cap<'a>) -> &'b mut [MDB<'a>] {
+    match cap {
+        &mut Cap::CPool(ref mut cpool) => {
+            &mut cpool.mdbs
+        }
+    }
+}
+
+fn match_mdbs<'a, 'b>(cap: &'b Cap<'a>) -> &'b [MDB<'a>] {
+    match cap {
+        &Cap::CPool(ref cpool) => {
+            &cpool.mdbs
+        }
+    }
+}
+
 impl<'a> MDB<'a> {
     pub fn derive(&'a mut self) -> MDB<'a> {
         MDB {
@@ -70,14 +86,6 @@ impl<'a> MDB<'a> {
         }
     }
 
-    fn match_mdbs<'b>(cap: &'b mut Cap<'a>) -> &'b mut [MDB<'a>] {
-        match cap {
-            &mut Cap::CPool(ref mut cpool) => {
-                &mut cpool.mdbs
-            }
-        }
-    }
-
     pub unsafe fn set(&mut self, addr: MDBAddr) {
         self.this = Some(addr.clone());
 
@@ -86,7 +94,7 @@ impl<'a> MDB<'a> {
             let mut prev = self.prev.clone().unwrap();
             let mut full_option = prev.cpool.write(prev.cpool_index);
             let full = full_option.as_mut().unwrap();
-            let ref mut mdbs = Self::match_mdbs(full);
+            let ref mut mdbs = match_mdbs_mut(full);
             mdbs[prev.mdb_index].next = Some(addr.clone());
         }
 
@@ -96,7 +104,7 @@ impl<'a> MDB<'a> {
             let mut current_child = current_child_option.clone().unwrap();
             let mut full_option = current_child.cpool.write(current_child.cpool_index);
             let full = full_option.as_mut().unwrap();
-            let ref mut mdbs = Self::match_mdbs(full);
+            let ref mut mdbs = match_mdbs_mut(full);
             mdbs[current_child.mdb_index].parent = Some(addr.clone());
             current_child_option = mdbs[current_child.mdb_index].next.clone();
         }
@@ -106,7 +114,7 @@ impl<'a> MDB<'a> {
             let mut next = self.next.clone().unwrap();
             let mut full_option = next.cpool.write(next.cpool_index);
             let full = full_option.as_mut().unwrap();
-            let ref mut mdbs = Self::match_mdbs(full);
+            let ref mut mdbs = match_mdbs_mut(full);
             mdbs[next.mdb_index].prev = Some(addr.clone());
         }
 
@@ -117,7 +125,7 @@ impl<'a> MDB<'a> {
                 let mut first_child = holding_parent.first_child.clone().unwrap();
                 let mut full_option = first_child.cpool.write(first_child.cpool_index);
                 let full = full_option.as_mut().unwrap();
-                let ref mut mdbs = Self::match_mdbs(full);
+                let ref mut mdbs = match_mdbs_mut(full);
                 mdbs[first_child.mdb_index].prev = Some(addr.clone());
             }
             self.next = holding_parent.first_child.clone();
@@ -127,75 +135,50 @@ impl<'a> MDB<'a> {
     }
 }
 
-// pub struct MDBChildIter<'a> {
-//     next_child: Option<MDBAddr>,
-//     current_cpool: &'a CPool<'a>,
-//     current_cpool_half: CPoolHalf,
-// }
+pub struct MDBChildIter<'a> {
+    next_child: Option<MDBAddr>,
+    _marker: PhantomData<Option<Cap<'a>>>
+}
 
-// impl<'a> Streamer<'a> for MDBChildIter<'a> {
-//     type Item = RefGuard<'a, Option<Cap<'a>>, IndexedSharedReadGuard<'a, Option<Cap<'a>>, usize, CPool<'a>>>;
+impl<'a> Iterator for MDBChildIter<'a> {
+    type Item = SharedReadGuard<'a, Option<Cap<'a>>>;
 
-//     fn next(&'a mut self) -> Option<Self::Item> {
-//         if self.next_child.is_some() {
-//             let next_child = self.next_child.take().unwrap();
-//             if next_child.cpool == self.current_cpool_half {
-//                 {
-//                     let cpool = self.current_cpool;
-//                     let ref mdb = cpool[next_child.cpool_index as usize].as_ref().unwrap().mdbs()[next_child.mdb_index];
-//                     self.next_child = mdb.next.clone();
-//                 }
-//                 Some(RefGuard::Ref(&self.current_cpool[next_child.cpool_index as usize]))
-//             } else {
-//                 {
-//                     let cpool = next_child.cpool.read();
-//                     let ref mdb = cpool[next_child.cpool_index as usize].as_ref().unwrap().mdbs()[next_child.mdb_index];
-//                     self.next_child = mdb.next.clone();
-//                 }
-//                 Some(RefGuard::Guard(IndexedSharedReadGuard::new(
-//                     next_child.cpool.read(),
-//                     next_child.cpool_index as usize
-//                 )))
-//             }
-//         } else {
-//             None
-//         }
-//     }
-// }
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next_child.is_some() {
+            let next_child = self.next_child.take().unwrap();
+            let full_option = next_child.cpool.read(next_child.cpool_index);
+            {
+                let full = full_option.as_ref().unwrap();
+                let ref mdbs = match_mdbs(full);
+                self.next_child = mdbs[next_child.mdb_index].next.clone();
+            }
+            Some(full_option)
+        } else {
+            None
+        }
+    }
+}
 
-// pub struct MDBChildIterMut<'a> {
-//     next_child: Option<MDBAddr>,
-//     current_cpool: &'a mut CPool<'a>,
-//     current_cpool_half: CPoolHalf,
-// }
+pub struct MDBChildIterMut<'a> {
+    next_child: Option<MDBAddr>,
+    _marker: PhantomData<Option<Cap<'a>>>
+}
 
-// impl<'a> Streamer<'a> for MDBChildIterMut<'a> {
-//     type Item = RefMutGuard<'a, Option<Cap<'a>>, IndexedSharedWriteGuard<'a, Option<Cap<'a>>, usize, CPool<'a>>>;
+impl<'a> Iterator for MDBChildIterMut<'a> {
+    type Item = SharedWriteGuard<'a, Option<Cap<'a>>>;
 
-//     fn next(&'a mut self) -> Option<Self::Item> {
-//         if self.next_child.is_some() {
-//             let mut next_child = self.next_child.take().unwrap();
-//             if next_child.cpool == self.current_cpool_half {
-//                 {
-//                     let ref cpool = self.current_cpool;
-//                     let ref mdb = cpool[next_child.cpool_index as usize].as_ref().unwrap().mdbs()[next_child.mdb_index];
-//                     self.next_child = mdb.next.clone();
-//                 }
-//                 let full = &mut self.current_cpool[next_child.cpool_index as usize];
-//                 Some(RefMutGuard::Ref(full))
-//             } else {
-//                 {
-//                     let cpool = next_child.cpool.read();
-//                     let ref mdb = cpool[next_child.cpool_index as usize].as_ref().unwrap().mdbs()[next_child.mdb_index];
-//                     self.next_child = mdb.next.clone();
-//                 }
-//                 Some(RefMutGuard::Guard(IndexedSharedWriteGuard::new(
-//                     next_child.cpool.write(),
-//                     next_child.cpool_index as usize
-//                 )))
-//             }
-//         } else {
-//             None
-//         }
-//     }
-// }
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next_child.is_some() {
+            let mut next_child = self.next_child.take().unwrap();
+            let full_option = next_child.cpool.write(next_child.cpool_index);
+            {
+                let full = full_option.as_ref().unwrap();
+                let ref mdbs = match_mdbs(full);
+                self.next_child = mdbs[next_child.mdb_index].next.clone();
+            }
+            Some(full_option)
+        } else {
+            None
+        }
+    }
+}
